14b81baf0371986dae24d3310cc4798ea37b640f
mjbowden <mjbowden@quinnipiac.edu>
2015-09-29 19:16:26 +0000

    Add question update view

--- qandu_app/core/views.py
+++ qandu_app/core/views.py
@@ -4,6 +4,7 @@ from django.core.urlresolvers import reverse_lazy
 from .models import *
 from django.views.generic import ListView
 from django.views.generic import DetailView
+from django.views.generic import UpdateView
 # Create your views here.
 class Home(TemplateView):
     template_name = "home.html"
@@ -22,3 +23,7 @@ class QuestionListView(ListView):
 class QuestionDetailView(DetailView):
     model = Question
     template_name = 'question/question_detail.html'
+class QuestionUpdateView(UpdateView):
+      model = Question
+      template_name = 'quextion/question_form.html'
+      fields = ['title', 'description']
