4cafbff260e3c9eaa41f477a3016a8aa8c01aa6f
mjbowden <mjbowden@quinnipiac.edu>
2015-10-01 18:48:07 +0000

    Add answer update view

--- qandu_app/core/views.py
+++ qandu_app/core/views.py
@@ -14,7 +14,7 @@ class QuestionCreateView(CreateView):
   template_name = "question/question_form.html"
   fields = ['title', 'description']
   success_url = reverse_lazy('question_list')
-  
+
   def form_valid(self, form):
     form.instance.user = self.request.user
     return super(QuestionCreateView, self).form_valid(form)
@@ -42,10 +42,18 @@ class AnswerCreateView(CreateView):
     model = Answer
     template_name = "answer/answer_form.html"
     fields = ['text']
-    
+
     def get_success_url(self):
         return self.object.question.get_absolute_url()
     def form_valid(self, form):
         form.instance.user = self.request.user
         form.instance.question = Question.objects.get(id=self.kwargs['pk'])
         return super(AnswerCreateView, self).form_valid(form)
+class AnswerUpdateView(UpdateView):
+    model = Answer
+    pk_url_kwarg = 'answer_pk'
+    template_name = 'answer/answer_form.html'
+    fields = ['text']
+
+    def get_sucess_url(self):
+        return self.object.question.get_absolute_url()
